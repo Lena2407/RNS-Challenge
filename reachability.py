3c225cde091308c850511caa139439f7ab271b5d
Luna Niehaus <lgniehaus@gmx.de>
2025-02-13 02:46:27 +0100

    Wokring with diagrams
    Co-authored-by: Lena2407 <Lena2407@users.noreply.github.com>

--- reachability.py
+++ reachability.py
@@ -10,12 +10,24 @@ p2p = {}
 ixp = set()
 all_ases = set()
 
+as_to_name = {8075:"Microsoft", 15169:"Google", 16509:"Amazon", 36351:"IBM", 
+              174:"Cogent", 209:"Centurylink", 286:"KPN", 701:"VZ Business", 1239:"Sprint", 1299:"Telia", 2828:"VZ", 2914:"NTT", 
+              3257:"GTT", 3320:"D Telekom", 3356:"Level 3", 3491:"PCCW", 5511:"Orange", 6453:"Tata", 6461:"Zayo", 6762:"IT SParkle", 
+              6830:"Liberty Global", 7018:"AT&T", 12956:"Telefonica", 6939:"HE", 7713:"TELIN PT", 3491:"PCCW", 4826:"Vocus", 
+              9002:"RETN", 1221:"Telstra", 7922:"Comcast", 4134:"CN Net", 
+              4766:"Korea Tele", 1257:"KCOM", 3292:"TDC", 22652:"Fibrenoire", 8001:"Stealth", 1273:"Vodafone", 
+              2497:"IIJapan", 6830:"Lib. Glob.", 2856:"Brit. Tele", 1257:"Tele2", 2516:"KDDI", 
+              7713:"PT", 2711:"Spirit", 12182:"Internap", 4589:"Easynet", 38930:"FibreRing"}
+
+
+
 # predefined ASNs
 cloud = [8075, 15169, 16509, 36351]
-tier1 = [174, 209, 286, 701, 1239, 1299, 2828, 2914, 3257, 3320, 3356, 3491, 5511, 6453, 6461, 6762, 6830, 7018, 12956]
+# same for 2020 and 2024 (3491 and 6453 are ne)
+#tier1 =      [174, 209, 286, 701, 1239, 1299, 2828, 2914, 3257, 3320, 3356, 3491, 5511, 6453, 6461, 6762, 6830, 7018, 12956]
+#tier1_2015 = [174, 209, 286, 701, 1239, 1299, 2828, 2914, 3257, 3320, 3356,       5511,       6461, 6762, 6830, 7018, 12956]
 tier2 = [6939, 7713, 3491, 4826, 9002, 1221, 7922, 4134, 4766, 1257, 3292, 22652, 8001, 1273, 2497, 6830, 2856, 1257, 2516, 7713, 2711, 12182, 4589, 38930]
 
-as_to_name = {}
 
 # pt 3243(viel zu niedrig), 7713(etwas zu hoch)
 # spirit 2711 (zu niedrig)
@@ -117,19 +129,10 @@ def reachability(bypass=[]):
     return reachable
 
                   
-read_as_relationship("Data/20200901.as-rel2.txt")
+read_as_relationship("Data/20151201.as-rel2.txt")
 provider_free = reachability()
 tier_1_free = reachability([1])
 hierarchy_free = reachability([1,2])
-print("Cloud:")
-for a in cloud:
-    print(a, len(provider_free[a]))
-print("Tier-1:")
-for a in tier1:
-    print(a, len(tier_1_free[a]))
-print("Tier-2:")
-for a in tier2:
-    print(a, len(hierarchy_free[a]))
 
 plotable_ases = []
 for a in types:
@@ -140,16 +143,16 @@ for a in types:
     plotable_ases.append(ases_reachable)
 
 plotable_ases.sort(key=lambda x: x[3], reverse=True)
-print(plotable_ases)
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Beispiel-Daten
-labels = [a[0] for a in plotable_ases]
+label_ases = [a[0] for a in plotable_ases]
+labels = [as_to_name[i] for i in label_ases]
 n_groups = len(labels)
-
+print(len(types))
 # Drei Werte für jede Säule (hinterster Wert ist der größte)
 values1 = np.array([a[3] for a in plotable_ases])  # Vorderster Wert
 values2 = np.array([a[2] for a in plotable_ases])  # Mittlerer Wert
@@ -158,7 +161,7 @@ values3 = np.array([a[1] for a in plotable_ases])  # Hinterster Wert
 color_dict = {0:"blue", 1:"red", 2:"green"}
 
 # Farben für jede Säule (Basisfarben für vorderste Werte)
-base_colors = [color_dict[types[i]] for i in labels]
+base_colors = [color_dict[types[i]] for i in label_ases]
 
 fig, ax = plt.subplots(figsize=(12, 6))
 bar_width = 0.85
@@ -180,7 +183,7 @@ for i in range(n_groups):
 # Achsenbeschriftungen
 ax.set_xlabel("Network reachablility")
 ax.set_ylabel("Number of ASes reachable")
-ax.set_title("Network reachability on CAIDA dataset from 2020")
+ax.set_title("Network reachability on CAIDA dataset from 2015")
 ax.set_xticks(np.arange(n_groups))
 ax.set_xticklabels(labels, rotation=90)
 
